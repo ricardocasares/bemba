bb2ca65499e04e73fe1061766ffa9797d5d21de3
Ricardo Casares <ricardocasares@gmail.com>
2018-05-10 11:51:34 +0200

    Remove grid

--- components/Search/index.js
+++ components/Search/index.js
@@ -2,13 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import Pad from "../Pad";
 
-const Grid = styled.div`
-  display: grid;
-  align-items: center;
-  justify-items: center;
-  grid-template-columns: 2fr 32px;
-`;
-
 const Input = styled.input`
   border: none;
   color: #fff;
