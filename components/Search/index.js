89229c804f2473e2d71db61a70e2eb9493360681
Ricardo Casares <ricardo_casares@epam.com>
2018-05-11 14:13:49 +0200

    Get rid of Pad

--- components/Search/index.js
+++ components/Search/index.js
@@ -2,20 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import { withRouter } from "next/router";
 import { connect } from "react-redux";
-
+// store
 import { searchDebounced } from "../../store/search/actions";
 
-import Pad from "../Pad";
-
-export let Search = ({ router, dispatch, query, searchDebounced }) => (
-  <Pad bg={"transparent"}>
-    <Input
-      placeholder="Start typing..."
-      defaultValue={query}
-      onChange={e => router.push(`/search?query=${e.target.value}`)}
-    />
-  </Pad>
-);
+let Container = styled.div`
+  padding: 15px;
+  background: transparent;
+`;
 
 let Input = styled.input`
   border: none;
@@ -26,6 +19,16 @@ let Input = styled.input`
   font-size: 18px;
   width: 100%;
 `;
+
+export let Search = ({ router, dispatch, query, searchDebounced }) => (
+  <Container>
+    <Input
+      placeholder="Start typing..."
+      defaultValue={query}
+      onChange={e => router.push(`/search?query=${e.target.value}`)}
+    />
+  </Container>
+);
 
 export default connect(({ search }) => ({ ...search }), {
   searchDebounced
