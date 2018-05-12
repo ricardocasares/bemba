af3acc9ebbaad00519e6139dd466105a975fd1f1
Ricardo Casares <ricardocasares@gmail.com>
2018-05-12 09:14:37 +0200

    Player WIP

--- components/Player/index.js
+++ components/Player/index.js
@@ -1,27 +1,57 @@
 import React from "react";
+import { pure } from "recompose";
 import styled from "styled-components";
 
 import Play from "../Button/Play";
-import Favorite from "../Button/Favorite";
-import Station from "../StationList/Station";
+import Pause from "../Button/Pause";
 
-let Player = styled.div`
+import Station from "../StationList/Station";
+import { PureComponent } from "react";
+
+export class Player extends PureComponent {
+  play = () => {
+    this.audio.play();
+  };
+  pause = () => this.audio.pause();
+
+  componentDidMount() {
+    let player = this.audio;
+
+    player.addEventListener("play", console.log);
+    player.addEventListener("playing", console.log);
+    player.addEventListener("pause", console.log);
+  }
+
+  render() {
+    let { className } = this.props;
+
+    return (
+      <div className={className}>
+        <Station
+          name="FM Pop & Rock"
+          state="Buenos Aires"
+          country="Argentina"
+        />
+        <Pause onClick={this.pause} />
+        <Play filled onClick={this.play} />
+        <audio
+          autoPlay={true}
+          ref={e => {
+            this.audio = e;
+          }}
+          src={"http://streaming.radionomy.com/2ROCK"}
+        />
+      </div>
+    );
+  }
+}
+
+export default styled(Player)`
   display: flex;
-  flex: 1 0 auto;
-  justify-items: left;
-  align-items: center;
   padding: 15px;
   background: #333;
-`;
 
-let FlexStation = styled(Station)`
-  flex: 1 1 auto;
+  > div {
+    flex: 1 1 auto;
+  }
 `;
-
-export default ({ ...props }) => (
-  <Player>
-    <FlexStation />
-    <Favorite />
-    <Play filled />
-  </Player>
-);
