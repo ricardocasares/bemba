b63dd9d28334168e291f4c00454f0718cc43d99f
Ricardo Casares <ricardocasares@gmail.com>
2018-05-12 09:14:28 +0200

    Deploy analytics

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,23 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="theme-color" content="#222" />
+          <meta
+            name="google-site-verification"
+            content="oDrI2nIuNzWJUxr9GOW2DmfSpkMAlLdBrpNvBdUufAc"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-214398-13');`
+            }}
+          />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-214398-13"
+          />
           {this.props.styleTags}
         </Head>
         <body>
