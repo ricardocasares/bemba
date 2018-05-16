6500478dd3a48f6973654c45618ec9b981ecad2f
Ricardo Casares <ricardocasares@gmail.com>
2018-05-16 02:31:41 +0200

    Add subtle animations

--- components/Button/Add.js
+++ components/Button/Add.js
@@ -1,11 +1,27 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 import Button from "./";
 import Plus from "../Icons/Plus";
 
+let enter = keyframes`
+  0% {
+    opacity: 0;
+    transform: rotate(0deg);
+  }
+
+  100% {
+    opacity: 1;
+    transform: rotate(-90deg);
+  }
+`;
+
+let AnimatedButton = Button.extend`
+  animation: ${enter} 0.3s linear;
+`;
+
 export default ({ ...props }) => (
-  <Button {...props}>
+  <AnimatedButton {...props}>
     <Plus />
-  </Button>
+  </AnimatedButton>
 );
--- components/Button/Remove.js
+++ components/Button/Remove.js
@@ -1,11 +1,25 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 import Button from "./";
 import Checkmark from "../Icons/Checkmark";
 
+let enter = keyframes`
+  0% {
+    transform: rotate(90deg);
+  }
+
+  100% {
+    transform: rotate(0);
+  }
+`;
+
+let AnimatedButton = Button.extend`
+  animation: ${enter} 0.1s linear;
+`;
+
 export default ({ ...props }) => (
-  <Button {...props}>
+  <AnimatedButton {...props}>
     <Checkmark color={"lime"} />
-  </Button>
+  </AnimatedButton>
 );
