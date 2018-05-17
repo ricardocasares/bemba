2ccb1330ccbb8d71194d4238c72a35cb0cc4cb0d
Ricardo Casares <ricardo_casares@epam.com>
2018-05-17 12:49:54 +0200

    Organize serve

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-let next = require("next");
-let polka = require("polka");
-let { join } = require("path");
-let dir = join(__dirname, "static");
-let static = require("serve-static")(dir);
-const { NODE_ENV, PORT = 3000 } = process.env;
+const next = require("next");
+const polka = require("polka");
+const { join } = require("path");
+const static = require("serve-static");
 
-let dev = NODE_ENV !== "production";
-let app = next({ dev });
-let handle = app.getRequestHandler();
-let render = name => (req, res) =>
+const { NODE_ENV, PORT = 3000 } = process.env;
+const dir = join(__dirname, "static");
+const dev = NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const render = name => (req, res) =>
   app.render(req, res, name, { ...req.query, ...req.params });
 
 app.prepare().then(() =>
   polka()
-    .use(static)
+    .use(static(dir))
     .get("/", render("/"))
     .get("/search", render("/search"))
     .get("/library", render("/library"))
