031813fe537e3de0c8e8b847bac3210888a4e830
Ricardo Casares <ricardocasares@gmail.com>
2018-05-26 17:25:10 +0200

    Add basic radio microsite

--- pages/station.tsx
+++ pages/station.tsx
@@ -1,20 +1,36 @@
 import React, { PureComponent } from "react";
+import styled from "styled-components";
 import { connect } from "react-redux";
 
 import { get } from "../store/station/actions";
+import { load } from "../store/player/actions";
 import { add, remove } from "../store/library/actions";
 
 import Seo from "../components/Seo";
-import Title from "../components/Text/Title";
 import Subtitle from "../components/Text/Subtitle";
 import Content from "../components/Content";
+import StationItem from "../components/Station";
+import Swiper from "../components/Swiper";
+import CardLink, { Card } from "../components/Card";
+import Add from "../components/Button/Add";
+import Play from "../components/Button/Play";
+import Remove from "../components/Button/Remove";
 
 interface StationProps {
   add?: Function;
   remove?: Function;
+  load?: Function;
   station: any;
   library: any;
 }
+
+const Toolbar = styled.div`
+  display: flex;
+
+  div:first-child {
+    flex: 1;
+  }
+`;
 
 class Station extends PureComponent<StationProps> {
   static async getInitialProps({ store, query }) {
@@ -23,21 +39,65 @@ class Station extends PureComponent<StationProps> {
   }
 
   render() {
-    const { station } = this.props;
+    const { load, add, remove, station, library } = this.props;
+    const { language } = station;
+    const tags = station.tags.split(",").filter(x => x);
+    const location = [station.country, station.state].filter(x => x);
+    const [country, state] = location;
+
     return (
       <Content>
         <Seo
-          title={[station.name, "Radio Bemba"].join(" - ")}
-          description={[
-            station.name,
-            ...[station.country, station.state].filter(x => x),
-            station.tags.split(",").join(" ")
-          ].join(" ")}
+          title={[station.name, location.join(", "), "Radio Bemba"].join(" - ")}
+          description={[station.name, location.join(", "), tags.join(" ")].join(
+            " "
+          )}
         />
-        <Title>{station.name}</Title>
-        <Subtitle>
-          {[station.country, station.state].filter(x => x).join(", ")}
-        </Subtitle>
+        <Toolbar>
+          <StationItem {...station} />
+          <Play onClick={() => load(station)} />
+          {library[station.id] ? (
+            <Remove onClick={() => remove(station.id)} />
+          ) : (
+            <Add onClick={() => add(station)} />
+          )}
+        </Toolbar>
+        <Subtitle>Find similar radios</Subtitle>
+        <Swiper>
+          {tags.map(tag => (
+            <CardLink
+              key={tag}
+              href={`/stations?filter=tags&name=${tag}`}
+              as={`/tags/${tag}`}
+            >
+              {tag}
+            </CardLink>
+          ))}
+          {language ? (
+            <CardLink
+              href={`/stations?filter=language&name=${language}`}
+              as={`/language/${language}`}
+            >
+              {language}
+            </CardLink>
+          ) : null}
+          {country ? (
+            <CardLink
+              href={`/stations?filter=country&name=${country}`}
+              as={`/country/${country}`}
+            >
+              {country}
+            </CardLink>
+          ) : null}
+          {state ? (
+            <CardLink
+              href={`/stations?filter=state&name=${state}`}
+              as={`/state/${state}`}
+            >
+              {state}
+            </CardLink>
+          ) : null}
+        </Swiper>
       </Content>
     );
   }
@@ -48,4 +108,4 @@ const mapProps = ({ station, library }) => ({
   library: library.stations
 });
 
-export default connect<StationProps>(mapProps, { add, remove })(Station);
+export default connect<StationProps>(mapProps, { add, remove, load })(Station);
