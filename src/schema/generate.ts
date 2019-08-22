ddb8e5b67b65952c664dcad72c47d518df4e28fc
Ricardo Casares <ricardocasares@gmail.com>
2019-08-22 02:39:17 +0200

    types

--- src/schema/generate.ts
+++ src/schema/generate.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import { makeSchema } from "nexus";
-import * as types from "./schema";
+import * as types from "./types";
 
 export const schema = makeSchema({
   types,
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,5 +1,5 @@
 import { makeSchema } from "nexus";
-import * as types from "./schema";
+import * as types from "./types";
 
 export const schema = makeSchema({
   types,
