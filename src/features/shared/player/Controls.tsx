538b39475092f447d4b6622019d5468b304be37d
Ricardo Casares <ricardo.casares@grandparade.co.uk>
2019-08-27 19:45:26 +0200

    fix: imports

--- src/features/shared/player/Controls.tsx
+++ src/features/shared/player/Controls.tsx
@@ -1,9 +1,9 @@
 import React, { FC, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { BembaState } from '@/models/state';
 import { Like } from './Like';
 import { Play } from './Play';
 import { Pause } from './Pause';
-import { connect } from 'react-redux';
-import { BembaState } from '@/models/state';
 
 type ControlsBox = {
   playing: boolean;
