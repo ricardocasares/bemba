3d2076a13792dee9a693486b793834a5ca20abe8
Ricardo Casares <ricardocasares@gmail.com>
2020-10-28 03:25:07 +0100

    fix: skeleton

--- src/components/Skeleton/index.tsx
+++ src/components/Skeleton/index.tsx
@@ -3,14 +3,13 @@ import Loader from "react-content-loader";
 export const CardSkeleton = () => (
   <Loader
     speed={2}
-    width={"100%"}
-    height={250}
-    viewBox="0 0 100% 250"
+    width="100%"
+    height={180}
     backgroundColor="var(--accents-1)"
     foregroundColor="var(--accents-2)"
   >
-    <rect x="0" y="0" width="100%" height="180" />
-    <rect x="0" y="190" width="60%" height="20" />
-    <rect x="0" y="220" width="30%" height="20" />
+    <rect x="0" y="0" width="100%" height="120" />
+    <rect x="0" y="125" width="60%" height="20" />
+    <rect x="0" y="150" width="30%" height="20" />
   </Loader>
 );
