0ed9dd3650371afeaea8f6626ca71bc4c4a83079
Ricardo Casares <ricardocasares@gmail.com>
2020-10-29 15:32:33 +0100

    fix: add page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,15 @@ import { Layout } from "@/components/Layout";
 import { Header } from "@/components/Header";
 import { Scrollable } from "@/components/Scrollable";
 import { Player } from "@/components/Player";
+import Head from "next/head";
 
 export function App({ Component, pageProps }: AppProps) {
   return (
     <Providers>
       <Global styles={styles} />
+      <Head>
+        <title>Radio Bemba</title>
+      </Head>
       <Layout>
         <Header />
         <Scrollable>
