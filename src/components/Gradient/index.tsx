3bbf388585e5f94b8b8f84bd87acabec23cce2aa
Ricardo Casares <ricardocasares@gmail.com>
2020-11-01 02:41:36 +0100

    fix: gradient component

---
+++ src/components/Gradient/index.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import gradient from "random-gradient";
+
+export type Gradient = {
+  hash?: string;
+  direction?: "radial" | "vertical" | "horizontal";
+};
+
+export const Gradient: FC<Gradient> = ({
+  hash,
+  direction = "vertical",
+  ...props
+}) => (
+  <div
+    style={{
+      minWidth: "100%",
+      minHeight: "120px",
+      background: gradient(ensureHash(hash), direction),
+    }}
+    {...props}
+  />
+);
+
+const ensureHash = (hash?: string) => hash || Math.random();
