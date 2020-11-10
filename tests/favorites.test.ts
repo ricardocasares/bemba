617fddfc8807e3b255216e5cdcb882d585f6b33e
Ricardo Casares <84963+ricardocasares@users.noreply.github.com>
2020-11-10 20:15:39 +0100

    tests: refactor (#17)
    * tests: add testcafe
    
    * tests: add github workflow
    
    * tests: os and node  matrix
    
    * docs: readme
    
    * tests: favorites page
    
    * test: job name
    
    * test: job name
    
    * tests: refactor

--- tests/favorites.test.ts
+++ tests/favorites.test.ts
@@ -1,31 +1,24 @@
 import { Selector } from "testcafe";
 
+const Card = Selector("[data-test-card]");
+const CardButton = Selector("[data-test-card-fave-button]");
+
 fixture`Favorites`.page(process.env.TARGET_URL).beforeEach(async (t) => {
-  const button = Selector("[data-test-card-fave-button]");
-  const id = await button.getAttribute("data-test-card-fave-button");
-
-  t.ctx.id = id;
-
-  return t.click(button).navigateTo("/favorites");
+  t.ctx.id = await CardButton.getAttribute("data-test-card-fave-button");
+  return t.click(CardButton).navigateTo("/favorites");
 });
 
-test("Adds a station to favorites", async (t) => {
-  const faved = Selector(`[data-test-card="${t.ctx.id}"]`);
-
-  return t
-    .expect(faved.exists)
+test("Adds a station to favorites", async (cafe) =>
+  cafe
+    .expect(Card.withAttribute("data-test-card", cafe.ctx.id).exists)
     .eql(true)
-    .expect(faved.count)
-    .eql(1);
-});
+    .expect(Card.count)
+    .eql(1));
 
-test("Removes a station from favorites", async (t) => {
-  const button = Selector(`[data-test-card-fave-button="${t.ctx.id}"]`);
-
-  return t
-    .click(button)
-    .expect(button.exists)
+test("Removes a station from favorites", async (cafe) =>
+  cafe
+    .click(CardButton.withAttribute("data-test-card-fave-button", cafe.ctx.id))
+    .expect(CardButton.exists)
     .eql(false)
-    .expect(button.count)
-    .eql(0);
-});
+    .expect(CardButton.count)
+    .eql(0));
