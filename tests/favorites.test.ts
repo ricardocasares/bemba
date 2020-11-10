e4508fa6712da08eb00011f814b53514d6bdcca3
Ricardo Casares <ricardocasares@gmail.com>
2020-11-10 19:17:17 +0100

    tests: favorites page

--- tests/favorites.test.ts
+++ tests/favorites.test.ts
@@ -1,15 +1,31 @@
 import { Selector } from "testcafe";
 
-fixture`Favorites`.page(process.env.TARGET_URL);
+fixture`Favorites`.page(process.env.TARGET_URL).beforeEach(async (t) => {
+  const button = Selector("[data-test-card-fave-button]");
+  const id = await button.getAttribute("data-test-card-fave-button");
+
+  t.ctx.id = id;
+
+  return t.click(button).navigateTo("/favorites");
+});
 
 test("Adds a station to favorites", async (t) => {
-  const button = Selector("[data-test-card-fave-button]").nth(1);
-  const id = await button.getAttribute("data-test-card-fave-button");
-  const faved = Selector(`[data-test-card="${id}"]`);
+  const faved = Selector(`[data-test-card="${t.ctx.id}"]`);
+
+  return t
+    .expect(faved.exists)
+    .eql(true)
+    .expect(faved.count)
+    .eql(1);
+});
+
+test("Removes a station from favorites", async (t) => {
+  const button = Selector(`[data-test-card-fave-button="${t.ctx.id}"]`);
 
   return t
     .click(button)
-    .navigateTo("/favorites")
-    .expect(faved.exists)
-    .eql(true);
+    .expect(button.exists)
+    .eql(false)
+    .expect(button.count)
+    .eql(0);
 });
