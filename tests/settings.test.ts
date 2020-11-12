66406f22b6a8310836135ca08abf1383ea1ff9a9
Ricardo Casares <ricardocasares@gmail.com>
2020-11-12 22:41:14 +0100

    tests: settings page

--- tests/settings.test.ts
+++ tests/settings.test.ts
@@ -1,30 +1,38 @@
 import { Selector } from "testcafe";
 
 const Html = Selector("html");
+const Link = Selector("li");
+const Title = Selector("h3");
 const Toggle = Selector("button");
 
 fixture`Settings`.page`${process.env.TARGET_URL}/settings`;
 
-test("Sets dark color mode", async (cafe) =>
-  cafe
-    .expect(Toggle.withText("Dark").exists)
-    .ok()
-    .click(Toggle.withText("Dark"))
-    .expect(Html.getAttribute("data-theme"))
-    .eql("dark"));
+[
+  ["Dark", "dark"],
+  ["Light", "light"],
+  ["System", "system"],
+].forEach(([label, mode]) =>
+  test(`Sets ${mode} color mode`, async (cafe) =>
+    cafe
+      .expect(Toggle.withText(label).exists)
+      .ok()
+      .click(Toggle.withText(label))
+      .expect(Html.getAttribute("data-theme"))
+      .eql(mode))
+);
 
-test("Sets light color mode", async (cafe) =>
-  cafe
-    .expect(Toggle.withText("Light").exists)
-    .ok()
-    .click(Toggle.withText("Light"))
-    .expect(Html.getAttribute("data-theme"))
-    .eql("light"));
-
-test("Sets system color mode", async (cafe) =>
-  cafe
-    .expect(Toggle.withText("System").exists)
-    .ok()
-    .click(Toggle.withText("System"))
-    .expect(Html.getAttribute("data-theme"))
-    .eql("system"));
+[
+  ["English", "Settings"],
+  ["Español", "Configuración"],
+  ["Polski", "Ustawienia"],
+  ["Française", "Réglages"],
+  ["Русский", "Настройки"],
+].forEach(([lang, label]) =>
+  test(`Sets application language to '${lang}'`, async (cafe) =>
+    cafe
+      .expect(Link.withText(lang).exists)
+      .ok()
+      .click(Link.withText(lang))
+      .expect(Title.withText(label).exists)
+      .ok())
+);
