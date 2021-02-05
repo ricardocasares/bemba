c83f948717532522a7b49743846c2b8090fffd93
Ricardo Casares <84963+ricardocasares@users.noreply.github.com>
2021-02-05 22:04:40 +0100

    feat: add google analytics plugin (#38)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react'
 import { AppProps } from "next/app";
+import { useRouter } from 'next/router'
 import { DefaultSeo } from "next-seo";
 import { Global } from "@emotion/react";
 import { Providers } from "@/lib/providers";
@@ -7,9 +9,19 @@ import { Header } from "@/components/Header";
 import { Scrollable } from "@/components/Scrollable";
 import { Player } from "@/components/Player";
 import { styles } from "@/css/global";
+import { pageview } from "@/lib/utils";
 import SEO from "@/config/seo";
 
 export function BembaApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => pageview(url);
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => router.events.off('routeChangeComplete', handleRouteChange)
+  }, [router.events])
+
   return (
     <Providers>
       <Global styles={styles} />
