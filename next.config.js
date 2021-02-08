eb81825165306a1e423eebe62e9b21ef8c0da68e
Ricardo Casares <84963+ricardocasares@users.noreply.github.com>
2021-02-09 00:44:14 +0100

    feat: analytics plugin (#43)

--- next.config.js
+++ next.config.js
@@ -5,5 +5,9 @@ module.exports = {
   },
   env: {
     NEXT_PUBLIC_SHA: process.env.VERCEL_GITHUB_COMMIT_SHA,
+    NEXT_PUBLIC_GA_TRACKING_ID: process.env.NEXT_PUBLIC_GA_TRACKING_ID,
   },
+  experimental: {
+    plugins: true
+  }
 };
