a032b80f00637b6062c97e02dd4ad4dc3d2b3b66
Ricardo Casares <ricardocasares@gmail.com>
2021-02-09 01:04:40 +0100

    fix: commit sha

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     defaultLocale: "en",
   },
   env: {
-    NEXT_PUBLIC_SHA: process.env.VERCEL_GITHUB_COMMIT_SHA,
+    NEXT_PUBLIC_SHA: process.env.VERCEL_GIT_COMMIT_SHA,
     NEXT_PUBLIC_GA_TRACKING_ID: process.env.NEXT_PUBLIC_GA_TRACKING_ID,
   },
   experimental: {
