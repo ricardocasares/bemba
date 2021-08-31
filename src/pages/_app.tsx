33d57e88cf63774923c2f786443dec7a9624a636
Ricardo Casares <84963+ricardocasares@users.noreply.github.com>
2021-08-31 17:01:51 +0200

    feat: use google analytics script (#183)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,28 @@
+import { useEffect } from "react";
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { DefaultSeo } from "next-seo";
 import { Providers } from "@/lib/providers";
 import { Layout } from "@/components/Layout";
 import { Header } from "@/components/Header";
 import { Scrollable } from "@/components/Scrollable";
 import { Player } from "@/components/Player";
-import seoProps from "@/config/seo";
 import { globalStyles } from "@/css/global";
+import { pageview } from "@/lib/utils";
+import seoProps from "@/config/seo";
+
 
 export function BembaApp({ Component, pageProps }: AppProps) {
   globalStyles();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => pageview(url);
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [router.events]);
 
   return (
     <Providers>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,22 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head>
           <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
