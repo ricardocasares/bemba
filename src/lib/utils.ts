cf5c50eeaba9c115974eba63ae25f28a4a822735
Ricardo Casares <84963+ricardocasares@users.noreply.github.com>
2022-02-11 21:44:56 +0100

    fix: language mapping

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,9 @@
 export const localesMap = {
-  en: "English",
-  es: "Español",
-  fr: "Française",
-  pl: "Polski",
-  ru: "Русский",
+  en: "english",
+  es: "spanish",
+  fr: "french",
+  pl: "polish",
+  ru: "russian",
 };
 
 export function mapValueToRange(
